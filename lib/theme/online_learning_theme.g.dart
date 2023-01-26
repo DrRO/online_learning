a34cd8cb7a92b07db0942b9dc99f98979df8d56e
Parabeac-Nest <admin@parabeac.com>
2023-01-26 08:51:19 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/online_learning_theme.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:online_learning/theme/online_learning_text_styles.g.dart';
+import 'package:online_learning/theme/online_learning_colors.g.dart';
+
+class OnlineLearningTheme {
+
+static const TextTheme textTheme = TextTheme(headline1: OnlineLearningTextStyles.headline1,headline2: OnlineLearningTextStyles.headline2,headline3: OnlineLearningTextStyles.headline3,headline4: OnlineLearningTextStyles.headline4,headline5: OnlineLearningTextStyles.headline5,headline6: OnlineLearningTextStyles.headline6,);
+
+static const ColorScheme light = ColorScheme.light(onPrimary: OnlineLearningColors.lightOnPrimary,primary: OnlineLearningColors.lightPrimary,primaryContainer: OnlineLearningColors.lightPrimaryContainer,secondary: OnlineLearningColors.lightSecondary,onSecondary: OnlineLearningColors.lightOnSecondary,onPrimaryContainer: OnlineLearningColors.lightOnPrimaryContainer,secondaryContainer: OnlineLearningColors.lightSecondaryContainer,onSecondaryContainer: OnlineLearningColors.lightOnSecondaryContainer,shadow: OnlineLearningColors.lightShadow);
+
+static  ThemeData themeDataLight = ThemeData(textTheme: textTheme, colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: OnlineLearningColors.darkPrimary,onPrimary: OnlineLearningColors.darkOnPrimary,primaryContainer: OnlineLearningColors.darkPrimaryContainer,onPrimaryContainer: OnlineLearningColors.darkOnPrimaryContainer,secondary: OnlineLearningColors.darkSecondary,onSecondary: OnlineLearningColors.darkOnSecondary,secondaryContainer: OnlineLearningColors.darkSecondaryContainer,onSecondaryContainer: OnlineLearningColors.darkOnSecondaryContainer,shadow: OnlineLearningColors.darkShadow);
+
+static  ThemeData themeDataDark = ThemeData(textTheme: textTheme, colorScheme: dark,);
+}
